e2df3e9ceeba9bf0a1371b9f8fa204b76409719e
anubhav230 <anubhavsingh230@gmail.com>
2020-06-09 18:12:58 +0530

    checking condition for player mavement

--- Snake_And_Ladder.sh
+++ Snake_And_Ladder.sh
@@ -1,6 +1,28 @@
 #!/bin/bash -x
-numberOfPlayer=1
-startPosition=0
+#constant
+FORWARD_MOVE=1
+BACKWARD_MOVE=2
+NO_MOVE=0
+NUMBER_OF_PLAYER=1
 
-diceRoller=$((RANDOM%6+1))
-echo "$diceRoller"
+#variables
+startPosition=0
+position=0
+movingCheck=$((RANDOM%3))
+stepCheck=$((RANDOM%6+1))
+case $movingCheck in
+	$FORWARD_MOVE)
+		position=$((startPotion+stepCheck))
+		echo "player moved $position steps forward"
+		;;
+	$BACKWARD_MOVE)
+		position=$((startPositon+stepCheck))
+		echo "player moved $position steps backward"
+		;;
+	$NO_MOVE)
+		position=$((startPosition+0))
+		echo "player on same position"
+
+		;;
+esac
+
