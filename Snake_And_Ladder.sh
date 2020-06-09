1c65d4c47e0682d1dae8dae2acfd3f151248e144
anubhav230 <anubhavsingh230@gmail.com>
2020-06-10 03:14:13 +0530

    start playing with2 players

--- Snake_And_Ladder.sh
+++ Snake_And_Ladder.sh
@@ -8,44 +8,102 @@ WINNING_POSITION=100
 START_POSITION=0
 
 #variables
-position=0
-
-positon=$startPosition
-rollDice() {
-	stepCheck=$((RANDOM%6+1))
-	movingCheck=$((RANDOM%3))
+declare -a player1Position
+declare -a player2Position
+playerOnePosition=$START_POSITION
+playerTwoPosition=$START_POSITION
+count1=0
+count2=0
+playerOne() {
+        stepCheck=$((RANDOM%6+1))
+        movingCheck=$((RANDOM%3))
         case $movingCheck in
                 $FORWARD_MOVE)
-                        position=$(($position+$stepCheck))
-			if [ $position -gt $WINNING_POSITION ]
-			then
-				position=$((position-stepCheck))
-			fi
+                        playerOnePosition=$(($playerOnePosition+$stepCheck))
+                        if [ $playerOnePosition -gt $WINNING_POSITION ]
+                        then
+                                playerOnePosition=$((playerOnePosition-stepCheck))
+                        fi
                         ;;
                 $BACKWARD_MOVE)
-                        position=$(($position-$stepCheck))
-                        if [ $position -lt $START_POSITION ]
+                        playerOnePosition=$(($playerOnePosition-$stepCheck))
+                        if [ $playerOnePosition -lt $START_POSITION ]
                         then
-                                position=$START_POSITION
+                                playerOnePosition=$START_POSITION
                         fi
                         ;;
                 $NO_MOVE)
-                       position=$(($position+0))
+                       playerOnePosition=$(($playerOnePosition+0))
                        ;;
         esac
 
-
+        player1Position[((count1++))]=$playerOnePosition
+        ((dice1++))
 
 }
 
-while (( $position < $WINNING_POSITION ))
+playerTwo() {
+        stepCheck=$((RANDOM%6+1))
+        movingCheck=$((RANDOM%3))
+        case $movingCheck in
+                $FORWARD_MOVE)
+                        playerTwoPosition=$(($playerTwoPosition+$stepCheck))
+                        if [ $playerTwoPosition -gt $WINNING_POSITION ]
+                        then
+                                playerTwoPosition=$((playerTwoPosition-stepCheck))
+                        fi
+                        ;;
+                $BACKWARD_MOVE)
+                        playerTwoPosition=$(($playerTwoPosition-$stepCheck))
+                        if [ $playerTwoPosition -lt $START_POSITION ]
+                        then
+                                playerTwoPosition=$START_POSITION
+                        fi
+                        ;;
+                $NO_MOVE)
+                       position=$(($playerTwoPosition+0))
+                       ;;
+        esac
+        player2Position[((count2++))]=$playerTwoPosition
+
+
+
+        ((dice2++))
+}
+
+
+
+while [[ $playerOnePosition -lt $WINNING_POSITION && $playerTwoPosition -lt $WINNING_POSITION ]]
 do
 
-	rollDice
-	((dice++))
-	echo "Player position=  $position  at $dice dice"
+        playerOne
+        while [ $movingCheck -eq $FORWARD_MOVE ]
+        do
+                        playerOne
+        done
 
+        playerTwo
+        while [ $movingCheck -eq $FORWARD_MOVE ]
+        do
+                        playerTwo
+        done
 
 done
-echo "player won the game $position"
-echo "dice has been rolled for $dice times"
+
+for print1 in ${player1Position[@]}
+do
+        echo player one position is -----// $print1
+done
+
+for print2 in ${player2Position[@]}
+do
+        echo player two position is ------// $print2
+done
+echo "player one total dice roll is $dice1"
+echo "player two total dice roll is $dice2"
+if [ $playerOnePosition -eq $WINNING_POSITION ]
+then
+        echo "=============================player one is winner============================="
+else
+        echo "=============================player two is winner============================="
+fi
