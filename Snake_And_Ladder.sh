0bc3ac540890e4ae1c6b7f4c953c3ed81d0b28b5
anubhav230 <anubhavsingh230@gmail.com>
2020-06-09 21:29:08 +0530

    winning condition check for one player

--- Snake_And_Ladder.sh
+++ Snake_And_Ladder.sh
@@ -1,28 +1,43 @@
-#!/bin/bash -x
+#!/bin/bash
 #constant
 FORWARD_MOVE=1
 BACKWARD_MOVE=2
 NO_MOVE=0
 NUMBER_OF_PLAYER=1
+WINNING_POSITION=100
+START_POSITION=0
 
 #variables
-startPosition=0
 position=0
-movingCheck=$((RANDOM%3))
-stepCheck=$((RANDOM%6+1))
-case $movingCheck in
-	$FORWARD_MOVE)
-		position=$((startPotion+stepCheck))
-		echo "player moved $position steps forward"
-		;;
-	$BACKWARD_MOVE)
-		position=$((startPositon+stepCheck))
-		echo "player moved $position steps backward"
-		;;
-	$NO_MOVE)
-		position=$((startPosition+0))
-		echo "player on same position"
 
-		;;
-esac
+positon=$startPosition
+rollDice() {
+	stepCheck=$((RANDOM%6+1))
+	movingCheck=$((RANDOM%3))
+        case $movingCheck in
+                $FORWARD_MOVE)
+                        position=$(($position+$stepCheck))
+                        echo "$position"
+                        ;;
+                $BACKWARD_MOVE)
+                        position=$(($position-$stepCheck))
+                        if [ $position -lt $START_POSITION ]
+                        then
+                                position=$START_POSITION
+                        fi
+                        echo "$position"
+                        ;;
+                $NO_MOVE)
+                       position=$(($position+0))
+                       ;;
+        esac
 
+
+}
+
+while (( $position < $WINNING_POSITION ))
+do
+	rollDice
+
+done
+echo "player won the game $position"
